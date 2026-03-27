bea7b6b7c4551b298687b1e660c12bb82552f352
muhasfi <muhammadhasfi15@gmail.com>
2026-03-27 17:25:16 +0700

    fix link

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function openRandomMeja(event) {
   const meja = Math.floor(Math.random() * 15) + 1;
 
   // buka link dengan meja random
-  const url = `https://restoran-git-main-muhasfis-projects.vercel.app/menu?meja=${meja}`;
+  const url = `restoran-taupe.vercel.app/menu?meja=${meja}`;
   window.open(url, "_blank");
 }
 function closeDrawer() {
